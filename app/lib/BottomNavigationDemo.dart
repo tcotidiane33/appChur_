222519451b6d4e94772ce0caceb4571a58a358d3
eureka <tcotidiane33@gmail.com>
2023-05-26 16:22:03 +0000

    add menu

--- app/lib/BottomNavigationDemo.dart
+++ app/lib/BottomNavigationDemo.dart
@@ -1,171 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:animations/animations.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:gallery/demos/material/material_demo_types.dart';
-import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
 
-class BottomNavigationDemo extends StatefulWidget {
-  const BottomNavigationDemo({
-    Key key,
-    @required this.restorationId,
-    @required this.type,
-  }) : super(key: key);
+/// Flutter code sample for [BottomNavigationBar].
 
-  final String restorationId;
-  final BottomNavigationDemoType type;
+void main() => runApp(const BottomNavigationBarExampleApp());
+
+class BottomNavigationBarExampleApp extends StatelessWidget {
+  const BottomNavigationBarExampleApp({super.key});
 
   @override
-  _BottomNavigationDemoState createState() => _BottomNavigationDemoState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BottomNavigationBarExample(),
+    );
+  }
 }
 
-class _BottomNavigationDemoState extends State<BottomNavigationDemo>
-    with RestorationMixin {
-  final RestorableInt _currentIndex = RestorableInt(0);
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
 
   @override
-  String get restorationId => widget.restorationId;
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
 
-  @override
-  void restoreState(RestorationBucket oldBucket, bool initialRestore) {
-    registerForRestoration(_currentIndex, 'bottom_navigation_tab_index');
-  }
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Welcome Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Welcome to give money',
+      style: optionStyle,
+    ),
+    Text(
+      'Welcome to learn at church School',
+      style: optionStyle,
+    ),
+    Text(
+      'Welcome to Settings',
+      style: optionStyle,
+    ),
+  ];
 
-  @override
-  void dispose() {
-    _currentIndex.dispose();
-    super.dispose();
-  }
-
-  String _title(BuildContext context) {
-    switch (widget.type) {
-      case BottomNavigationDemoType.withLabels:
-        return GalleryLocalizations.of(context)
-            .demoBottomNavigationPersistentLabels;
-      case BottomNavigationDemoType.withoutLabels:
-        return GalleryLocalizations.of(context)
-            .demoBottomNavigationSelectedLabel;
-    }
-    return '';
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final colorScheme = Theme.of(context).colorScheme;
-    final textTheme = Theme.of(context).textTheme;
-
-    var bottomNavigationBarItems = <BottomNavigationBarItem>[
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.add_comment),
-        label: GalleryLocalizations.of(context).bottomNavigationCommentsTab,
-      ),
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.calendar_today),
-        label: GalleryLocalizations.of(context).bottomNavigationCalendarTab,
-      ),
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.account_circle),
-        label: GalleryLocalizations.of(context).bottomNavigationAccountTab,
-      ),
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.alarm_on),
-        label: GalleryLocalizations.of(context).bottomNavigationAlarmTab,
-      ),
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.camera_enhance),
-        label: GalleryLocalizations.of(context).bottomNavigationCameraTab,
-      ),
-    ];
-
-    if (widget.type == BottomNavigationDemoType.withLabels) {
-      bottomNavigationBarItems = bottomNavigationBarItems.sublist(
-          0, bottomNavigationBarItems.length - 2);
-      _currentIndex.value = _currentIndex.value
-          .clamp(0, bottomNavigationBarItems.length - 1)
-          .toInt();
-    }
-
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(_title(context)),
+        title: const Text('BottomNavigationBar Sample'),
       ),
       body: Center(
-        child: PageTransitionSwitcher(
-          child: _NavigationDestinationView(
-            // Adding [UniqueKey] to make sure the widget rebuilds when transitioning.
-            key: UniqueKey(),
-            item: bottomNavigationBarItems[_currentIndex.value],
-          ),
-          transitionBuilder: (child, animation, secondaryAnimation) {
-            return FadeThroughTransition(
-              child: child,
-              animation: animation,
-              secondaryAnimation: secondaryAnimation,
-            );
-          },
-        ),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        showUnselectedLabels:
-            widget.type == BottomNavigationDemoType.withLabels,
-        items: bottomNavigationBarItems,
-        currentIndex: _currentIndex.value,
-        type: BottomNavigationBarType.fixed,
-        selectedFontSize: textTheme.caption.fontSize,
-        unselectedFontSize: textTheme.caption.fontSize,
-        onTap: (index) {
-          setState(() {
-            _currentIndex.value = index;
-          });
-        },
-        selectedItemColor: colorScheme.onPrimary,
-        unselectedItemColor: colorScheme.onPrimary.withOpacity(0.38),
-        backgroundColor: colorScheme.primary,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.teal,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
 }
-
-class _NavigationDestinationView extends StatelessWidget {
-  _NavigationDestinationView({Key key, this.item}) : super(key: key);
-
-  final BottomNavigationBarItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ExcludeSemantics(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.asset(
-                  'assets/demos/bottom_navigation_background.png',
-                  package: 'flutter_gallery_assets',
-                ),
-              ),
-            ),
-          ),
-        ),
-        Center(
-          child: IconTheme(
-            data: const IconThemeData(
-              color: Colors.white,
-              size: 80,
-            ),
-            child: Semantics(
-              label: GalleryLocalizations.of(context)
-                  .bottomNavigationContentPlaceholder(
-                item.label,
-              ),
-              child: item.icon,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
